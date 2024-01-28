0b7411465561dc8e0d0b209361c7ea2a15768f7c
jalalFaour <ahmadjalalfaour@gmail.com>
2024-01-28 06:39:03 +0300

    added absence time feature

--- controllers/course.controllers.js
+++ controllers/course.controllers.js
@@ -58,8 +58,8 @@ const getMyCourses = async (req, res, next) => {
 
             courses = editedCourses;
         }
-        print(editedCourses)
-        print(courses)
+        console.log(editedCourses)
+        console.log(courses);
         res.status(200).json(courses);
 
     } catch (error) {
